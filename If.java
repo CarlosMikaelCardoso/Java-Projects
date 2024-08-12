b9f6976b7bc7f78978ddcc4d04fca76c359c6c05
Carlos Mikael <mikael.cardoso.costa13@gmail.com>
2024-08-12 16:43:40 -0300

    If

---
+++ If.java
@@ -0,0 +1,16 @@
+public class if{
+    public static void main (String[] args){
+
+        int age = 18;
+
+        if (age == 18){
+            System.out.println("Ok boommer")
+        }
+        else if (age < 18){
+            System.out.println("Ok Boommer")
+        }
+        else {
+            System.out.println("Get Out!!!")
+        }
+    }
+}
