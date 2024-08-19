69349cee4a7ed00d1e6f8bd07113e3cd708cb303
Carlos Mikael <mikael.cardoso.costa13@gmail.com>
2024-08-19 10:49:07 -0300

    Bro Code Codes

---
+++ Human.java
@@ -0,0 +1,11 @@
+public class Human {
+    String name;
+    int age;
+    double weight;
+
+    Human (String name, int age, double weight){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+
+        Human human1 = new Human("Mikael", 19, 60);
+
+        System.out.println(human1.name);
+        System.out.println(human1.age);
+        System.out.println(human1.weight);
+    }
+}
